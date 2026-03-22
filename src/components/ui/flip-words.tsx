6c6b87b4d6eab28445a39c8f4526595fe74b1e14
hophamlam <hophamlam92@yahoo.com>
2026-03-22 17:07:54 +0700

    chore: downgrade Astro and related dependencies for compatibility and stability

--- src/components/ui/flip-words.tsx
+++ src/components/ui/flip-words.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
